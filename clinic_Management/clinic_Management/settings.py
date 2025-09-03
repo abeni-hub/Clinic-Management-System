3f86badee242d6ec39bd85c05eb1b843acf43234
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2025-09-03 12:36:11 -0700

    Update settings.py

--- clinic_Management/clinic_Management/settings.py
+++ clinic_Management/clinic_Management/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'clinic.apps.ClinicConfig',
     'corsheaders',
+    'django_filters',
 
 ]
 
@@ -139,6 +140,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+    ,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,  # default items per page
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ]
 ,
 }
 STATIC_URL = '/static/'  # URL that handles static files (e.g., http://127.0.0.1:8000/static/)
